3324c34316b6774211772d261fbafeaa7623623c
kamina <kamina@kamina>
2023-10-05 16:40:41 -0500

    Main

--- src/Main/Test.java
+++ src/Main/Test.java
@@ -15,7 +15,7 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        system.out.println("Hello World!");
+        System.out.println("Hello World!");
         //mmmmmmmmmm
     }
     
