308cca2ca734941a9b12a59a6207586e1e93ed34
kamina <kamina@kamina>
2023-10-05 16:53:32 -0500

    Main

--- src/Main/Test.java
+++ src/Main/Test.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package Main;
 
 /**
@@ -12,6 +9,7 @@ public class Test {
 
     /**
      * @param args the command line arguments
+     * @return Hello World!
      */
     public static void main(String[] args) {
         // TODO code application logic here
